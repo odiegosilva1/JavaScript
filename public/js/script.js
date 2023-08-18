a9cc295a0319384bb5a614ed249daf2ec2dfd90b
Diego Silva <odiegosilvadev@gmail.com>
2023-08-18 15:04:41 -0300

    create script

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+
+var n1 = Number(window.prompt('Digite um número: '))
+var n2 = Number(window.prompt('Digite outro número: '))
+var s = n1 + n2
+window.alert(`A Soma entre ${n1} e ${n2} é igual a ${s}`)
+
+//metodos
+
+/*
+var s = 'javaScript'
+s.length - Mostra quantos caracteres tem a string.
+s.toUpperCase()  - Transforma a string em letras maiusculas.
+s.toLowerCase - Joga tos os caracteres para minusculas.
+*/
+
