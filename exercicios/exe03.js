d98742fa7cd5734ab46eb159162a454dd6fdaae9
Diego Silva <odiegosilvadev@gmail.com>
2023-08-21 09:44:08 -0300

    update condições

---
+++ exercicios/exe03.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`A velocidade so seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
