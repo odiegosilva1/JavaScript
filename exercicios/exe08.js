69b094762bd1483d26c08a82d0b6f637e4a38674
Diego Silva <odiegosilvadev@gmail.com>
2023-08-22 10:12:44 -0300

    Create exe08.js

---
+++ exercicios/exe08.js
@@ -0,0 +1,23 @@
+//Condições multiplas 
+
+var agora = new Data()
+var diaSem = agora.getDay
+
+//Domingo indie - 0  
+
+switch(diaSem){
+    case 0:
+        console.log('Domingo')
+        break
+
+    case 1:
+        console.log('Segunda')
+        break
+
+    case 2:
+        console.log('Terça')
+        break
+
+    case 3:
+        console.log('Quarta')            
+}
